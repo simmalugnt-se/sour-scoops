e7baf1a63637d6a7ac65c62312af7fd12a584895
Jens Waern <jens@simmalugnt.se>
2023-01-25 11:01:17 +0100

    First Commit

---
+++ functions.php
@@ -0,0 +1,25 @@
+<?php
+// Load Tailwind from CDN
+// https://tailwindcss.com/docs/installation/play-cdn
+function load_tailwind()
+{
+    wp_enqueue_script('tailwind', 'https://cdn.tailwindcss.com', array(), null, false);
+    // Load tailwind.config.js after tailwind has been loaded
+    wp_enqueue_script('tailwind-config', get_template_directory_uri() . '/tailwind.config.js', array('tailwind'), null, false);
+}
+add_action('wp_enqueue_scripts', 'load_tailwind');
+
+// Register support for title-tag and theme menu
+add_action('after_setup_theme', function () {
+    add_theme_support('title-tag');
+    add_theme_support('menus');
+});
+
+// Register theme menu(s)
+// https://developer.wordpress.org/themes/functionality/navigation-menus/
+function register_theme_menus()
+{
+    register_nav_menus(array(
+        'primary-menu' => __('Primary Menu'),
+    ));
+}
