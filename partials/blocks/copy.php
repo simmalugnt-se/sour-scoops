311157e00b4ea47764a6a802b1d1545f336cfdfa
Jens Waern <jens@simmalugnt.se>
2023-01-26 14:11:05 +0100

    Center copy title

--- partials/blocks/copy.php
+++ partials/blocks/copy.php
@@ -4,7 +4,7 @@ $bg = $args['bg'] ?: 'bg-gray-100';
 ?>
 
 <div class="grid grid-cols-12 grid-rows-1 py-20 bg-<?= $bg; ?> border-y border-brown">
-    <div class="col-span-8 col-start-4">
+    <div class="col-span-8 col-start-2">
         <h2 class="text-3xl lg:text-4xl font-oi text-center mb-12"><?= $title; ?></h2>
         <div class="columns-2">
             <p>From "Rotting Garbage" to "Sour Lips" and "Gummy Worms and Ashes" our ice cream will tantalize your taste buds. Come taste the sourness for yourself and satisfy your cravings with our one-of-a-kind flavors.</p>
