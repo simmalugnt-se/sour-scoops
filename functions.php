99068e3169ff2fcd9d3a81c242e8f9ae16aa5810
Jens Waern <jens@simmalugnt.se>
2023-01-30 09:33:15 +0100

    Load Tailwind

--- functions.php
+++ functions.php
@@ -1,4 +1,32 @@
 <?php
+// Load Tailwind from CDN
+// https://tailwindcss.com/docs/installation/play-cdn
+function load_tailwind()
+{
+    wp_enqueue_script('tailwind', 'https://cdn.tailwindcss.com', array(), null, false);
+    // Load tailwind.config.js after tailwind has been loaded
+    wp_enqueue_script('tailwind-config', get_template_directory_uri() . '/tailwind.config.js', array('tailwind'), null, false);
+}
+add_action('wp_enqueue_scripts', 'load_tailwind');
+
+// Register theme support
+add_action('after_setup_theme', function () {
+    add_theme_support('title-tag');
+    add_theme_support('menus');
+});
+
+// Register theme menu(s)
+// https://developer.wordpress.org/themes/functionality/navigation-menus/
+function register_theme_menus()
+{
+    register_nav_menus(array(
+        'primary-menu' => __('Primary Menu'),
+        'footer-menu' => __('Footer Menu'),
+    ));
+}
+add_action('init', 'register_theme_menus');
+
+// Print array or object in a pre tag
 function print_a($data)
 {
 ?>
