b775d3764ebfb80a3d7fff41e3bdc395bbffe481
Jens Waern <jens@simmalugnt.se>
2023-01-30 09:16:17 +0100

    Empty files for lesson-01

--- index.php
+++ index.php
@@ -1,13 +1,16 @@
 <?php get_header(); ?>
 
 
-<div class="grid grid-cols-2 gap-4 p-8 max-w-7xl mx-auto">
-    <?php if (have_posts()) : ?>
+<?php if (have_posts()) : ?>
 
-        <?php while (have_posts()) : the_post(); ?>
-            <?php get_template_part('partials/blocks/post-card', 'post-card', array()); ?>
-        <?php endwhile; ?>
+    <?php while (have_posts()) : the_post(); ?>
+        <pre>
+            <code>
+                <?php print_r($post()); ?>
+            </code>
+        </pre>
+    <?php endwhile; ?>
 
-    <?php endif; ?>
-</div>
+<?php endif; ?>
+
 <?php get_footer(); ?>
--- page.php
+++ page.php
@@ -1,11 +1,12 @@
 <?php get_header(); ?>
 
-<?php get_template_part('partials/blocks/hero', 'hero', array('title' => "Icecreams so sour, they'll make your lips pucker")); ?>
 
-<div class="max-w-7xl mx-auto">
-    <h2><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
-    <?php the_content(); ?>
-</div>
+<?php if (have_posts()) : ?>
 
+    <?php while (have_posts()) : the_post();
+        print_a($post);
+    endwhile; ?>
+
+<?php endif; ?>
 
 <?php get_footer(); ?>
