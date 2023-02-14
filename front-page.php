4031335faa9018548207baface94382ed75a056e
Jens Waern <jens@simmalugnt.se>
2023-02-14 09:42:13 +0100

    Display latest posts on landingpage

--- front-page.php
+++ front-page.php
@@ -1,20 +1,30 @@
-<?php get_header(); ?>
+<?php
+get_header();
+$latest_news = new WP_Query(
+    array(
+        'post_type' => 'post',
+        'posts_per_page' => 3,
+        'orderby' => 'date',
+        'order' => 'DESC'
+    )
+);
+?>
 
 
 <?php get_template_part('partials/blocks/hero', 'hero', array('title' => "Icecreams so sour, they'll make your lips pucker")); ?>
 <?php get_template_part('partials/blocks/copy', 'copy', array('title' => "Welcome to Sour Scoops, where unique and daring flavors reign supreme", "bg" => "blue-dark")); ?>
 <?php get_template_part('partials/blocks/bestseller-grid', 'bestseller-grid', array('title' => "Bestsellers")); ?>
 <?php get_template_part('partials/blocks/locations-highlight', 'locations-highlight', array('title' => "Bestsellers")); ?>
-<?php /*if (have_posts()) : ?>
-
-    <?php while (have_posts()) : the_post(); ?>
-
-        <h2><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
-
-        <?php the_excerpt(); ?>
-
-    <?php endwhile; ?>
-
-<?php endif;*/ ?>
+<?php if ($latest_news->have_posts()) : ?>
+    <div class="grid grid-cols-1 md:grid-cols-3 gap-4 p-8 max-w-7xl mx-auto">
+        <div class="col-span-full flex flex-col md:flex-row justify-between items-center mb-6">
+            <h2 class="text-5xl lg:text-6xl font-oi">Latest News</h2>
+            <a class="bg-white border border-brown rounded-lg py-2 px-5 inline-block mt-4" href="<?= get_permalink(get_option('page_for_posts')); ?>">Read more news 👉</a>
+        </div>
+        <?php while ($latest_news->have_posts()) : $latest_news->the_post();
+            get_template_part('partials/blocks/post-card', 'post-card', array());
+        endwhile; ?>
+    </div>
+<?php endif; ?>
 
 <?php get_footer(); ?>
--- partials/blocks/post-card.php
+++ partials/blocks/post-card.php
@@ -3,11 +3,11 @@
 
 <div class="flex flex-col gap-6">
     <div class="rounded-xl border border-brown overflow-hidden h-96 relative">
-        <img src="<?= get_template_directory_uri(); ?>/src/images/icecream.png" class="object-cover w-full h-full" />
-        <date class="absolute left-6 bottom-6 bg-white p-2 border border-brown rounded-lg"><?php echo get_the_date(); ?></date>
+        <?php the_post_thumbnail('large', array('class' => 'object-cover w-full h-full')); ?>
+        <date class="absolute left-6 bottom-6 bg-white p-2 border border-brown rounded-lg"><?= get_the_date(); ?></date>
     </div>
     <div class="rounded-xl bg-blue py-4 border border-brown">
-        <h3 class="text-3xl lg:text-4xl font-oi mb-12 px-5">Ice</h3>
-        <a class="block border-t border-brown px-5 pt-3" href="<?php the_permalink(); ?>">Read more 👉</a>
+        <h3 class="text-3xl lg:text-4xl font-oi mb-12 px-5"><?php the_title(); ?></h3>
+        <a class="block border-t border-brown px-5 pt-3" title="<?php the_title(); ?>" href="<?php the_permalink(); ?>">Read more 👉</a>
     </div>
 </div>
