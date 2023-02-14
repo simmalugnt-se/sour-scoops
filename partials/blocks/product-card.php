11cc91e8cd01329e38b2cb7535b88753b944869b
Jens Waern <jens@simmalugnt.se>
2023-02-14 10:18:38 +0100

    Use just one spline model in single-ice-cream and product-card

--- partials/blocks/product-card.php
+++ partials/blocks/product-card.php
@@ -1,23 +1,9 @@
-<?php
-$images = [
-    // "https://prod.spline.design/4UEHB9Sh1rGUkvXW/scene.splinecode",
-    // "https://prod.spline.design/C3Gfwb38TTg9Ucgb/scene.splinecode",
-    // "https://prod.spline.design/Vs-h0AQxoZHFmAsv/scene.splinecode",
-    // "https://prod.spline.design/5bGlJ-Ww4-ncU7Xl/scene.splinecode",
-    "https://prod.spline.design/LvRa4Y-edxnROzmX/scene.splinecode",
-    "https://prod.spline.design/LvRa4Y-edxnROzmX/scene.splinecode",
-    "https://prod.spline.design/pSltKjPMNRN5TeCm/scene.splinecode"
-];
-// Get random from $images
-$randomImage = $images[array_rand($images)];
-?>
-
 <div class="flex flex-col gap-6">
     <div class="rounded-xl  border border-brown overflow-hidden relative aspect-square bg-white">
-        <canvas class="absolute object-cover inset-0 w-full h-full" data-spline="<?= $randomImage; ?>"></canvas>
+        <canvas class="absolute object-cover inset-0 w-full h-full" data-spline="https://prod.spline.design/LvRa4Y-edxnROzmX/scene.splinecode"></canvas>
     </div>
     <div class="rounded-xl bg-blue py-4 border border-brown">
-        <h3 class="text-3xl lg:text-4xl font-oi mb-12 px-5">Ice</h3>
-        <a class="block border-t border-brown px-5 pt-3" href="">Read more 👉</a>
+        <h3 class="text-3xl lg:text-4xl font-oi mb-12 px-5"><?php the_title(); ?></h3>
+        <a class="block border-t border-brown px-5 pt-3" title="<?php the_title(); ?>" href="<?php the_permalink(); ?>">Read more 👉</a>
     </div>
 </div>
--- single-ice-cream.php
+++ single-ice-cream.php
@@ -3,7 +3,7 @@
 
 <div class="w-screen grid grid-cols-12 gap-6 grid-rows-1 max-w-7xl mx-auto p-8">
     <div class="col-span-6 border border-brown rounded-xl overflow-hidden bg-blue aspect-square">
-        <canvas data-spline="https://prod.spline.design/4UEHB9Sh1rGUkvXW/scene.splinecode" class="w-full h-full border"></canvas>
+        <canvas data-spline="https://prod.spline.design/LvRa4Y-edxnROzmX/scene.splinecode" class="w-full h-full border"></canvas>
     </div>
     <div class="col-span-6 flex flex-col gap-6 justify-center text-center px-6">
         <h1 class="font-oi text-4xl lg:text-7xl"><?php the_title(); ?></h1>
